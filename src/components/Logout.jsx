e02ca078f8d056c33bc4604404a3428bd2684c02
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-25 12:57:36 +0100

    feat: creating react header, footer and logout components

---
+++ src/components/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Logout = () => {
+  return (
+    <div className='logout'>
+        <h1>You are logged out!</h1>
+        <div>
+            Thank your for using our App. Come back soon!
+        </div>
+    </div>
+  )
+}
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -7,20 +7,28 @@ import { Login } from './Login';
 import { Welcome } from './Welcome';
 import { Error } from './Error';
 import { ListTodos } from "./ListTodos";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { Logout } from "./Logout";
 
 export const TodoApp = () => {
   return (
     <div className='todo-app'>
-        <BrowserRouter>
+      <Header />
+
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
           <Route path='/welcome/:username' element={<Welcome />} />
           <Route path='/todos' element={<ListTodos />} />
-          
+          <Route path='/logout' element={<Logout />} />
+
           <Route path='*' element={<Error />} />
         </Routes>
       </BrowserRouter>
+
+      <Footer />
     </div>
   )
 }
