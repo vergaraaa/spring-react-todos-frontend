1f81c52f1bcfce7f8840a2a6c34869e2e5385103
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-28 12:13:56 +0100

    feat: adding formik and moment to displau todo component

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getTodoFromUser } from '../api/TodoApiService';
 import { useAuthContext } from '../security/AuthContext';
+import { Formik, Form, Field } from 'formik';
 
 export const TodoComponent = () => {
 
@@ -9,22 +10,48 @@ export const TodoComponent = () => {
     const { username } = useAuthContext();
 
     const [description, setDescription] = useState("");
-    const [targetDate, setTargetDate] = useState(null)
+    const [targetDate, setTargetDate] = useState("")
     
     useEffect(() => {    
         getTodoFromUser(username, id) 
             .then((res) => { 
                 setDescription(res.data.description);
+                setTargetDate(res.data.targetDate);
             })
             .catch((error) => error);
-    }, [])
+    }, []);
     
+    const onSubmit = (values) => {
+        console.log(values);
+    }
 
   return (
     <div className='container'>
         <h1>Enter Todo Details</h1>
         <div>
-            <h3>{description}</h3>
+            <Formik initialValues={{
+                description,
+                targetDate,
+            }} enableReinitialize={true} onSubmit={onSubmit}>
+                {
+                    (props) => (
+                        <Form>
+                            <fieldset className='form-group'>
+                                <label htmlFor="description">Description</label>
+                                <Field type="text" className="form-control" name="description" />
+                            </fieldset>
+                            <fieldset className='form-group'>
+                                <label htmlFor="targetDate">Target date</label>
+                                <Field type="date" className="form-control" name="targetDate" />
+                            </fieldset>
+
+                            <div>
+                                <button className="btn btn-success my-3" type='submit'>Save</button>
+                            </div>
+                        </Form>
+                    )
+                }
+            </Formik>
         </div>
     </div>
   )
